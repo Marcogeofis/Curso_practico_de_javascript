b097d4c214d371bde9096d7a807ba9c1ded7dc61
Marco Rivera <marcogeofis@gmail.com>
2021-07-28 17:09:51 -0500

    En javascript ingrese las formulas del cuadrado y triángulo

---
+++ figuras.js
@@ -0,0 +1,26 @@
+// código del cuadrado 
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden:" + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es:" + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es:" + areaCuadrado + "cm");
+console.groupEnd();
+// código del triangulo
+console.group("Triángulo");
+const ladotriangulo1 = 6;
+const ladotriangulo2 = 6;
+const basetriangulo3 = 4;
+console.log("Los lados del triángulo miden:" 
++ ladotriangulo1 
++ "cm, " 
++ ladotriangulo2 
++ "cm, " 
++ basetriangulo3 
++ "cm"
+);
+
+console.groupEnd();
