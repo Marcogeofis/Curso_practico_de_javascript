721a1bb4b7a06caa525744853afe47ba02430f22
Marco Rivera <marcogeofis@gmail.com>
2021-08-03 20:58:34 -0500

    tenia un error en javascript, pero se soluciono, era error de palabra deje un comentario en javascript de ese error

--- tiendades.js
+++ tiendades.js
@@ -1,7 +1,3 @@
-// Haremos las funciones donde calcularemos el precio con su descuento de un producto
-// const precioOriginal = 120;
-// const descuento = 80;
-
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -9,20 +5,13 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioConDescuento;
 }
 function onclickButtonPriceDiscount(){
-    const inputPrice = document.getElementById("Inputprice");
-    const priceValue = inputPrice.Value;
-
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.Value;
-
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+   const inputPrice = document.getElementById("InputPrice");
+   const priceValue = inputPrice.value;  // anterior mente cuando corria el programa me marcaba un error como NaN
+                                         // Esto se debe a que no escribí bien la pabra (inputPrice.Value) la forma correcta es (inputPrice.value) value es en minuscula al inicio.                      
+   const inputDiscount = document.getElementById("InputDiscount");
+   const discountValue = inputDiscount.value;
+   //alert(precioConDescuento);
+   const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+   const resultP = document.getElementById("ResultP");
+   resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
-
-// console.log({
-    //   precioOriginal,
-    //   descuento,
-    //   porcentajePrecioConDescuento,
-    //   precioConDescuento,
-    // });
